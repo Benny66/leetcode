1242ed731f4ce1beea99c37f90072d550a27cb20
wenzhicong <wenzhicong@jasonanime.com>
2023-05-14 22:11:41 +0800

    LC:二叉树的层序遍历

---
+++ 树/levelOrder.go
@@ -0,0 +1,48 @@
+package tree
+
+/*
+二叉树的层序遍历
+给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
+输入：root = [3,9,20,null,null,15,7]
+输出：[[3],[9,20],[15,7]]
+*/
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func levelOrder(root *TreeNode) [][]int {
+	if root == nil {
+		return nil
+	}
+	ret := [][]int{}
+	q := []*TreeNode{root}
+	index := 0
+	for len(q) > 0 {
+		ret = append(ret, []int{})
+		p := []*TreeNode{}
+		for j := 0; j < len(q); j++ {
+			node := q[j]
+			ret[index] = append(ret[index], node.Val)
+			if node.Left != nil {
+				p = append(p, node.Left)
+			}
+			if node.Right != nil {
+				p = append(p, node.Right)
+			}
+		}
+		q = p
+		index++
+	}
+	return ret
+}
