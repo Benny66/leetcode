7c7e511f0ac178f41120771e5b2998df60ed722d
wenzhicong <wenzhicong@jasonanime.com>
2023-05-14 22:07:12 +0800

    LC:二叉树的最大深度

---
+++ 树/maxDepth.go
@@ -0,0 +1,44 @@
+package tree
+/*
+二叉树的最大深度
+给定一个二叉树，找出其最大深度。
+二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+说明: 叶子节点是指没有子节点的节点。
+示例：
+给定二叉树 [3,9,20,null,null,15,7]，
+返回它的最大深度 3 。
+
+
+*/
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	if root.Left == nil && root.Right == nil {
+		return 1
+	}
+	res := 1
+	n := maxDepth(root.Left)
+	m := maxDepth(root.Right)
+	if n > m {
+		res += n
+	} else {
+		res += m
+	}
+	return res
+}
